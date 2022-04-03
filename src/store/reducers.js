6148a53c41215397f3cdebf64d8fad777f3666aa
Julian Lopez <jlopezsa@gmail.com>
2022-04-02 22:31:31 -0500

    feat: create swhitch case in reducer function, and the reducer is imported in the index file

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,16 +1,19 @@
-
+// Store (Mi tienda inicial)
 const initialState = {
   count: 0,
 }
 
+// Reducer function
 function reducer(state = initialState, action){
   switch (action.type) {
     case 'ADD':
       return {
+        ...state, // crea una cópia de la tienda
         count: state.count + 1,
       }
       case 'REMOVE':
       return{
+        ...state, // crea uuna cópia de la tienda
         count: state.count - 1,
       }
     default:
